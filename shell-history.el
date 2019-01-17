e97940292c514d6259bcf97bfe1c4ad7b37456cf
Paulo Sousa <pauloj10@gmail.com>
2019-01-17 22:51:52 +0100

    Add caching to history

--- shell-history.el
+++ shell-history.el
@@ -12,11 +12,7 @@
 
 ;;; Code:
 
-(defun shell-history-get-history ()
-  "Executes a shell command to get the history"
-  (-> "HISTFILE=~/.bash_history && set -o history && history"
-      shell-command-to-string
-      (split-string "\n")))
+(defvar shell-history-cache)
 
 (defun shell-history-parse-history (history)
   "Gets a list of commands in raw formart and remove leading white spaces and history number"
@@ -25,6 +21,16 @@
                                     ""
                                     h))
         history))
+
+(defun shell-history-get-history! (&optional refresh)
+  "Executes a shell command to get the history"
+  (if (-> refresh not (and shell-history-cache))
+    shell-history-cache
+    (let ((history (-> "HISTFILE=~/.bash_history && set -o history && history"
+                       shell-command-to-string
+                       (split-string "\n")
+                       shell-history-parse-history)))
+      (setq shell-history-cache history))))
 
 (defun shell-history-build-helm-source (history)
   (helm-build-sync-source "Shell history: "
@@ -35,8 +41,7 @@
 (defun shell-history ()
   "Open shell history and insert the selected command in the buffer"
   (interactive)
-  (let* ((src (-> (shell-history-get-history)
-                  shell-history-parse-history
+  (let* ((src (-> (shell-history-get-history!)
                   shell-history-build-helm-source)))
     (helm :sources src)))
 
