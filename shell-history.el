fb3d158e730d4c7b39997b21dfa261ce421ce40e
Paulo Sousa <pauloj10@gmail.com>
2019-01-18 00:29:15 +0100

    User namespace-like in functions

--- shell-history.el
+++ shell-history.el
@@ -12,9 +12,9 @@
 
 ;;; Code:
 
-(defvar shell-history-cache)
+(defvar shell-history/cache)
 
-(defun shell-history-parse-history (history)
+(defun shell-history/parse-history (history)
   "Gets a list of commands in raw formart and remove leading white spaces and history number"
   (-map (lambda (h)
           (replace-regexp-in-string "[[:space:]]*[0-9][[:space:]]*"
@@ -22,17 +22,17 @@
                                     h))
         history))
 
-(defun shell-history-get-history! (&optional refresh)
+(defun shell-history/get-history! (&optional refresh)
   "Executes a shell command to get the history"
-  (if (-> refresh not (and shell-history-cache))
-    shell-history-cache
+  (if (-> refresh not (and shell-history/cache))
+    shell-history/cache
     (let ((history (-> "HISTFILE=~/.bash_history && set -o history && history"
                        shell-command-to-string
                        (split-string "\n")
-                       shell-history-parse-history)))
-      (setq shell-history-cache history))))
+                       shell-history/parse-history)))
+      (setq shell-history/cache history))))
 
-(defun shell-history-build-helm-source (history)
+(defun shell-history/build-helm-source (history)
   (helm-build-sync-source "Shell history: "
     :fuzzy-match t
     :candidates history
@@ -41,8 +41,8 @@
 (defun shell-history ()
   "Open shell history and insert the selected command in the buffer"
   (interactive)
-  (let* ((src (-> (shell-history-get-history!)
-                  shell-history-build-helm-source)))
+  (let* ((src (-> (shell-history/get-history!)
+                  shell-history/build-helm-source)))
     (helm :sources src)))
 
 (provide 'shell-history)
